7c8147c71a48152f7ea3753ad04d6c853c6c9ed4
jodawoon <jodw0103@naver.com>
2022-11-22 16:20:41 +0900

    Pboard-upload추가

---
+++ FRONT/js/Pboard-upload.js
@@ -0,0 +1,11 @@
+$(function cllick_border() {
+    $("img").click(function(){
+        $("img").css("border","none");
+        $(this).css("border","4px solid #396cfd");
+    })
+})
+
+function click_border() {
+    $("img").css("border","none");
+    $(this).css("border","4px solid #396cfd");
+}
